5ef97fa100adc08e6f3fb53d68f23c8d4473bd77
Ramilade <rami0904@stud.kea.dk>
2022-09-14 09:14:58 +0200

    Initial commit

---
+++ src/main/java/dk/kea/webformdemo/WebformdemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.webformdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebformdemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebformdemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dk/kea/webformdemo/WebformdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.webformdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebformdemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
