c314e8986ac08dae22ae77aa849e67ed5b9a888f
Ramilade <rami0904@stud.kea.dk>
2022-09-14 10:23:05 +0200

    Html

--- src/main/java/dk/kea/webformdemo/controller/WebFormController.java
+++ src/main/java/dk/kea/webformdemo/controller/WebFormController.java
@@ -2,9 +2,13 @@ package dk.kea.webformdemo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebFormController {
 
-
+@GetMapping("/")
+  public String showFor(){
+  return "webform";
+}
 }
