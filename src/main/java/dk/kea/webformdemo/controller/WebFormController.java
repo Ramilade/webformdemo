04e57e36ae78ba7462fc56bbd682030a0823afe9
Ramilade <rami0904@stud.kea.dk>
2022-09-14 19:32:57 +0200

    Html file 2 added

--- src/main/java/dk/kea/webformdemo/controller/WebFormController.java
+++ src/main/java/dk/kea/webformdemo/controller/WebFormController.java
@@ -2,7 +2,11 @@ package dk.kea.webformdemo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WebFormController {
@@ -11,4 +15,23 @@ public class WebFormController {
   public String showFor(){
   return "webform";
 }
+
+@PostMapping("/lavhilsen")
+  public String hilsenSide(@RequestParam("navn")String name, @RequestParam("alder")int age, RedirectAttributes redirectAttributes){
+  System.out.println(name + age); //erstattes med database create/update/delete
+  redirectAttributes.addAttribute("navn", name);
+  redirectAttributes.addAttribute("alder", age);
+
+  return "redirect:/lavhilsen";
+}
+@GetMapping("/lavhilsen")
+  public String lavHilsenSide(@RequestParam("navn")String name, @RequestParam("alder")int age, Model model){
+
+  model.addAttribute("navn",name);
+  model.addAttribute("alder", age);
+
+  return "hilsenside";
+
+}
+
 }
